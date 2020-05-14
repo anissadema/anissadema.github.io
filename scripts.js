f370cc78d00e38bf311758d87eb912674f3f99cb
anissadema <59878743+anissadema@users.noreply.github.com>
2020-05-14 14:50:49 +0200

    update

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
-
+var myArray = [
+    "Schiffausflug",
+    "Bambuddha",
+    "Sa Punta",
+    "Karting",
+    "Spanisch Vocabular lernen",
+    "Ausflug zu Nassau Beachclub"
+  ];
+  
+  var randomItem = myArray[Math.floor(Math.random()*myArray.length)];
+  
