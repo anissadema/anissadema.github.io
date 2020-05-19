477b2b83c0113a6384364872db0e8313bc16827f
anissadema <59878743+anissadema@users.noreply.github.com>
2020-05-19 13:14:10 +0200

    update

--- script.js
+++ script.js
@@ -107,11 +107,3 @@ function giveRandomItem (array) {
 
 
 
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
